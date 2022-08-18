62f3e18c4544cc04b5276a9ed09facc7e0f2a43e
unknown <aaron.pagoada@gmail.com>
2022-08-18 00:48:02 -0400

    Base code

---
+++ bot.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='w/')
+
+@bot.event
+async def on_ready():
+  try:
+    print("Logged in as " + bot.user.name)
+  except Exception:
+    print(Exception)
